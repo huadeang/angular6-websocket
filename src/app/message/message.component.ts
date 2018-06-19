7bb2a4aa67b384355b30a674bdb317640f5f91fe
Sangad Wandech <sangad@gmail.com>
2018-06-19 22:41:41 +0700

    animate when data change

--- src/app/message/message.component.ts
+++ src/app/message/message.component.ts
@@ -1,27 +1,70 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger,style,transition,animate,keyframes,query,stagger } from '@angular/animations';
 
 import { WebsocketService } from '../websocket.service';
 
 @Component({
   selector: 'app-message',
   templateUrl: './message.component.html',
-  styleUrls: ['./message.component.scss']
+  styleUrls: ['./message.component.scss'],
+  //animations
+  animations: [
+    trigger('listStagger', [
+      transition('* <=> *', [
+        query(
+          ':enter',
+          [
+            style({ opacity: 0, transform: 'translateY(-15px)' }),
+            stagger(
+              '50ms',
+              animate(
+                '550ms ease-out',
+                style({ opacity: 1, transform: 'translateY(0px)' })
+              )
+            )
+          ],
+          { optional: true }
+        ),
+        query(':leave', animate('50ms', style({ opacity: 0 })), {
+          optional: true
+        })
+      ])
+    ])
+  ],
+  
 })
 export class MessageComponent implements OnInit {
   serverMsg:string;
   msg:string;
-  url:string = 'ws:/localhost:8999';
+  message$:Array<any>;
+  startAnimate:boolean = false;
+  //url:string = 'ws:/localhost:8999';
 
   constructor(private wss:WebsocketService) { 
     
   }
 
   ngOnInit() {
-    this.wss.createObservableSocket(this.url)
+    this.wss.createObservableSocket()
     .subscribe(message=>{
       console.log(`Message ${message} received.`);
       this.serverMsg = message;
+      if(!this.message$){
+        this.message$ = new Array();
+      }
+      this.message$.push(message);
+      this.startAnimate = true;
     });
+  }
+
+  animationStarted($event){
+    console.log('Animation started.');
+    //this.animateStated = true;
+  }
+
+  animationDone($event){
+    console.log('Animation done.');
+    this.startAnimate = false;
   }
 
   send(){
--- src/app/websocket.service.ts
+++ src/app/websocket.service.ts
@@ -1,23 +1,49 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 
+// import * as jwt from 'jsonwebtoken';
+
+//import { WebSocket } from 'websocket';
+
+
 @Injectable({
   providedIn: 'root'
 })
 export class WebsocketService {
 
-  ws: WebSocket;
+  //ws: WebSocket;
+  ws : WebSocket;
+  url:string = 'ws:/localhost:8999';
+
+
 
   constructor() { }
 
-  createObservableSocket(url:string):Observable<string>{
+  createObservableSocket():Observable<string>{
+   /*
+    var token =jwt.sign({name:'bank'},'welcome',{
+      expiresIn : 60 * 60 * 1000 //expire in 1 hour
+    });
+    */
 
-    this.ws = new WebSocket(url);
+    var token = 'fake-token';
+
+    var msg = {
+      type: 'authenticate',
+      payload: { token: token }
+    };
+
+    this.ws = new WebSocket(this.url);
+    //this.ws = new WebSocketCilent()
 
     return new Observable(server=>{
       this.ws.onmessage = event=> server.next(event.data);
       this.ws.onerror = event => server.error(event);
       this.ws.onclose = event => server.complete();
+      /*this.ws.onopen = event =>{
+        this.ws.send(JSON.stringify(msg));
+      }
+      */
     });
   }
 
